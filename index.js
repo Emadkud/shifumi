ee70bfc14c892365083263ed1aaade01e3f30ede
Emad0007 <BASEMRF007@GMAIL.COM>
2022-07-05 16:40:16 +0100

    commit

--- index.js
+++ index.js
@@ -1,100 +1,74 @@
-"use strict"
-
+"use strict";
 
 const buttons = document.querySelectorAll("button");
 const username = prompt("Veuillez indiquer votre nom d'utilisateur");
 
-
-for (let i = 0; i < buttons.length; i++){
-  buttons[i].addEventListener("click", function() {
-    let joueur = buttons[i].innerHTML
-    let IA = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
-    let resultat
-
-    if (joueur === IA)
-      resultat = "Vous avez fait une egalité !"
-
-    else if ((joueur === "Pierre" && IA === "Ciseaux") || (joueur === "Feuille" && IA === "Pierre") || (joueur === "Ciseaux" && IA === "Feuille"))
-      resultat = "Vous avez gagné !"
-    else
-      resultat = "Vous avez perdu !";
-    document.querySelector(".resultat").innerHTML = `
-      ${username} : ${joueur}</br>
-     l'IA a choisi : ${IA}</br>
-      ${resultat}
-    `
-  })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const pierre = () => {
-//   document.getElementById("pierre")
-//   alert("le round est lancé")
-// }
-// const feuille = () => {
-//   document.getElementById("feuille")
-//   alert("le round est lancé")
-// }
-// const ciseaux = () => {
-//   document.getElementById("ciseaux")
-//   alert("le round est lancé")
-// }
-
-// let IA ;
-// let P2 ;
+let scorePlayer = 0;
+let scoreIa = 0;
+let roundPlayer = 0;
+let roundIa = 0;
  
-// const ppc = (IA, p2) => {
-//   if (IA === p2) {
+
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => {
+     
+    let joueur = buttons[i].innerHTML;
+    let IA = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
+    let resultat;
+
+    if (scorePlayer !== 3 || scoreIa !== 3) {
+
+      if (scorePlayer === 3) {
+        resultat = "vous avez gagné le jeu";
+      } 
+      else if (scoreIa === 3) {
+        resultat = "IA a gagné le jeu";
+        
+      }
+
+      else if (joueur === IA) {
+        resultat = "Vous avez fait une egalité !";
+        egalité.style.color='red';
+      }
+
+       else if (
+        (joueur === "Pierre" && IA === "Ciseaux") ||
+        (joueur === "Feuille" && IA === "Pierre") ||
+        (joueur === "Ciseaux" && IA === "Feuille"))
+       {
+        resultat = "Vous avez gagné !";
+        scorePlayer++;
+
+      } else if (
+        (IA === "Pierre" && joueur === "Ciseaux") ||
+        (IA === "Feuille" && joueur === "Pierre") ||
+        (IA === "Ciseaux" && joueur === "Feuille"))
+       
+      {
+        resultat = "Vous avez perdu !";
+        scoreIa++;
+      }
+      
+    
+      
+      else{
+        resultat('le jeu est fini');
+      }
+    } 
+
+    document.querySelector(".resultat").innerHTML = `
+      ${username}  : ${joueur}</br>
+       IA a choisi : ${IA}</br>
+      ${resultat}  </br>
+      score  ${username} : ${scorePlayer} <br>
+      score  IA : ${scoreIa}  <br>
+       
+     
+    `;
    
-//     return "égalité!"
-   
+    
 
-//   }
-
-//   if (IA === "pierre" && p2 === "ciseaux") {
-//     return "IA a gagné!"
-
-//   } else if (IA === "papier" && p2 === "pierre") {
-//     return "IA a gagné!"
-
-//   } else if (IA === "ciseaux" && p2 === "papier") {
-//     return "IA a gagné!"
-
-//   } else {
-//     return "p2 a gagné!"
-//   }
-// }
-
-// ppc("pierre", "pierre")
-// ppc("ciseaux", "pierre")
-// ppc("papier", "ciseaux")
-// ppc("papier", "papier")
+    
+  });
+}
